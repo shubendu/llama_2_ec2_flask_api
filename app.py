9f71279aa70a420bb383a206db0c348ecb1a4075
shubendu <biswasshubendu4@gmail.com>
2023-10-05 10:10:08 +0000

    first_commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Define the allowed file extensions
+ALLOWED_EXTENSIONS = {'txt'}
+
+# Function to check if the file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file_api():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
+
+    if file and file.filename.endswith('.txt'):
+        content = file.read()
+        #llm file
+        #lloutput
+        return jsonify({"content": content.decode('utf-8')})
+    else:
+        return jsonify({"error": "Invalid file format. Please upload a .txt file"})
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return "No selected file"
+    
+    if file and allowed_file(file.filename):
+        content = file.read()
+        return render_template('index.html', content=content)
+    else:
+        return "Invalid file format. Please upload a .txt file."
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
